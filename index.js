5153e773a00c8adfa29eeb15ab8daebdb6de9e40
Aniket1997 <16riju@gmail.com>
2024-07-20 16:48:52 +0530

    Ts Config file added

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+//@ts-ignore
+console.log("Hello Typescript");
+var myAge = 23;
+console.log("My age is", myAge);
+//write a code in typescript to add two numbers
+//@ts-ignore
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(10, 23));
---
+++ index.ts
@@ -0,0 +1,14 @@
+//@ts-ignore
+console.log("Hello Typescript");
+var myAge: number = 23;
+console.log("My age is", myAge);
+
+//write a code in typescript to add two numbers
+
+//@ts-ignore
+function sum (a:number,b:number):number
+{
+    return a + b;
+}
+
+console.log(sum(10,23));
